03b1adfc49c641170965f640677b328e62ce1f55
minhvhb <minhvhb@gmail.com>
2026-08-26 00:34:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if uploaded_file is not None:
                     """
 
                     response = client.models.generate_content(
-                        model='gemini-3.7-flash',
+                        model='gemini-3.6-flash',
                         contents=[types.Part.from_bytes(data=file_bytes, mime_type=mime_type), prompt]
                     )
                     
